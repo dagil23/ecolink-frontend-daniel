7c703bf290c6cecc0965984654b3c2344d75acb7
CristianCarbonell <cristian2306c@gmail.com>
2025-01-28 12:37:02 +0100

    updated navbar with offcanvas

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {CommonModule, NgOptimizedImage} from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 
@@ -10,7 +10,8 @@ import { FooterComponent } from './components/footer/footer.component';
     FooterComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    NgOptimizedImage
   ], exports: [
     HeaderComponent,
     FooterComponent
