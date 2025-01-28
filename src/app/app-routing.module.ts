f1887564e5608e64ecce6bcc08a117a96dacd8fd
CristianCarbonell <cristian2306c@gmail.com>
2025-01-28 14:55:51 +0100

    creating auth module and sections.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,8 @@ import { HomeComponent } from './home/components/home/home.component';
 const routes: Routes = [
   {
     path: '', component: HomeComponent
-  }
+  },
+  { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) }
 ];
 
 @NgModule({
