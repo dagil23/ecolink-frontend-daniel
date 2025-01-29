a10ecdef8c36ca5c8dc6a4143c3e46c110464baa
CarlosBertomeuC <carlos.bertomeu@iesdoctorbalmis.com>
2025-01-29 12:20:58 +0100

    feat: enhance missions component with improved styles and add Challenge model and service

---
+++ src/app/home/models/Challenge.ts
@@ -0,0 +1,6 @@
+export interface Challenge {
+    companyName: string;
+    description: string;
+    budget: number;
+    startDate: Date;
+  }
--- src/app/home/services/ChallengeService.service.ts
+++ src/app/home/services/ChallengeService.service.ts
@@ -1,10 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Challenge } from '../models/Challenge';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChallengeServiceService {
-
-  constructor() { }
-
+  url: string = 'http://localhost:8080/api/chalenges';
+  constructor(private http: HttpClient) { }
+  getChallenge(): Observable<Challenge[]> {
+    return this.http.get<Challenge[]>(this.url);
+  }
 }
