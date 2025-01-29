8b42cebba9588a81d21f7d03d5a59e69fd20ace8
CarlosBertomeuC <carlos.bertomeu@iesdoctorbalmis.com>
2025-01-29 10:40:20 +0100

    feat: implement carousel for challenges section with countdown timer

--- src/app/home/components/challenges/challenges.component.ts
+++ src/app/home/components/challenges/challenges.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component,OnInit, OnDestroy } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
+
 
 @Component({
   selector: 'home-challenges',
   templateUrl: './challenges.component.html',
   styleUrl: './challenges.component.scss'
 })
-export class ChallengesComponent {
 
+export class ChallengesComponent implements OnInit, OnDestroy {
+  timeLeft: number = 7 * 24 * 60 * 60; // 7 días en segundos
+  private timerSubscription!: Subscription;
+
+  ngOnInit() {
+    this.startTimer();
+  }
+
+  ngOnDestroy() {
+    if (this.timerSubscription) {
+      this.timerSubscription.unsubscribe();
+    }
+  }
+
+  startTimer() {
+    this.timerSubscription = interval(1000).subscribe(() => {
+      if (this.timeLeft > 0) {
+        this.timeLeft--;
+      }
+    });
+  }
+
+  formatTime(time: number): string {
+    const days = Math.floor(time / (24 * 60 * 60));
+    const hours = Math.floor((time % (24 * 60 * 60)) / (60 * 60));
+    const minutes = Math.floor((time % (60 * 60)) / 60);
+    const seconds = time % 60;
+    return `${days}d ${hours}h ${minutes}m ${seconds}s`;
+  }
 }
