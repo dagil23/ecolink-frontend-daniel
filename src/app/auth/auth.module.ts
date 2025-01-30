00ee1e85326e549367b89b6cfe45243a7f5c26b4
CristianCarbonell <cristian2306c@gmail.com>
2025-01-30 16:55:29 +0100

    improved navbar with new distribution and profile icon.

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -6,6 +6,7 @@ import { AuthComponent } from './auth.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import {ReactiveFormsModule} from '@angular/forms';
+import {SharedModule} from '../shared/shared.module';
 
 
 @NgModule({
@@ -17,7 +18,8 @@ import {ReactiveFormsModule} from '@angular/forms';
   imports: [
     CommonModule,
     AuthRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    SharedModule
   ]
 })
 export class AuthModule { }
