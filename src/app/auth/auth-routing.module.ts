f076e3d532c9ff706a5118f597afeda3709291af
Lorenzo <ls.krastev@iesdoctorbalmis.com>
2025-01-30 12:32:47 +0100

    refactor: comment out registration route in auth-routing module

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,15 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './auth.component';
-import { RegisterComponent } from './components/register/register.component';
 
 const routes: Routes = [
   {
     path: '', component: AuthComponent
   },
-  {
-    path: 'register', component: RegisterComponent
-  }
+  // {
+  //   path: 'register', component: RegisterComponent
+  // }
 ];
 
 @NgModule({
