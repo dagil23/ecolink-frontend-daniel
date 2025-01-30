cbf5f7f5b03331462f7504d79567835a5c91f6fc
Lorenzo <ls.krastev@iesdoctorbalmis.com>
2025-01-30 12:47:13 +0100

    feat: implement About Us routing and add Ods service

---
+++ src/app/services/ods-service.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from '../environments/environment';
+import { Observable } from 'rxjs';
+import { Ods } from '../home/models/Ods';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OdsServiceService {
+  private baseUrl: string = environment.apiUrl + '/ods';
+
+  constructor(private http: HttpClient) { }
+
+  getOds(): Observable<Ods[]> {
+    return this.http.get<Ods[]>(this.baseUrl);
+  }
+}
