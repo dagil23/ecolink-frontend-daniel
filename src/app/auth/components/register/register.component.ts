2e4317ec26af459611c0cca91f1f82f62437a5f9
CristianCarbonell <cristian2306c@gmail.com>
2025-01-30 12:29:51 +0100

    Temporal fix

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+/*import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
@@ -67,3 +67,5 @@ export class RegisterComponent implements OnInit {
     }, 2000);
   }
 }
+
+ */
