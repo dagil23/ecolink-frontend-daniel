c049e056dde6d133b85c157d29bd15f731913b6c
CristianCarbonell <cristian2306c@gmail.com>
2025-01-30 10:07:08 +0100

    Restructured auth folder

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,8 +3,8 @@ import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { AuthComponent } from './auth.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 
 @NgModule({
