9e3cac09261528ccfa5a0bbbf21763e7c59de9cf
CristianCarbonell <cristian2306c@gmail.com>
2025-01-31 07:54:30 +0100

    starting implementation of login template.

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './auth.component';
 import { RegisterComponent } from './components/register/register.component';
+import {LoginComponent} from './components/login/login.component';
+
 
 
 const routes: Routes = [
@@ -9,8 +11,10 @@ const routes: Routes = [
     path: '', component: AuthComponent
   },
   {
-    path: 'register', component: RegisterComponent
-
+    path: 'register', component: RegisterComponent,
+  },
+  {
+    path: 'login', component: LoginComponent
   }
 ];
 
--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {LoginService} from '../../services/LoginService.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
+  isSubmitting = false;
 
+  constructor(private fb: FormBuilder, private loginService: LoginService) {}
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+    });
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.invalid) return;
+
+    this.isSubmitting = true;
+    console.log('Login form submitted:', this.loginForm.value);
+
+    this.loginService.login(this.loginForm.value).subscribe(
+      response => {
+        this.isSubmitting = false;
+        alert('Login successful');
+      },
+      error => {
+        this.isSubmitting = false;
+        alert('Login failed');
+      }
+    );
+  }
 }
