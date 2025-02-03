5f90a2e74e02976115f056fc48f6e72cbcd9376a
Lorenzo <ls.krastev@iesdoctorbalmis.com>
2025-02-03 12:15:27 +0100

    feat: add filter component and integrate startup name in product listing

--- src/app/product-page/product-page.module.ts
+++ src/app/product-page/product-page.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 import { ProductPageRoutingModule } from './product-page-routing.module';
 import { ProductsComponent } from './products/products.component';
 import { SharedModule } from "../shared/shared.module";
+import { FilterComponent } from './filter/filter.component';
 
 
 @NgModule({
   declarations: [
-    ProductsComponent
+    ProductsComponent,
+    FilterComponent
   ],
   imports: [
     CommonModule,
