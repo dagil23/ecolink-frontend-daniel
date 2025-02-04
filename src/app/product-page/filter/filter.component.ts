3cb389083489fac940ec0916edd79481c9d12fa2
CarlosBertomeuC <carlos.bertomeu@iesdoctorbalmis.com>
2025-02-04 12:49:55 +0100

    feat: add filtering functionality to product page with event emitter for filtered products

--- src/app/product-page/filter/filter.component.ts
+++ src/app/product-page/filter/filter.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { ProductService } from '../../services/ProductService.service';
+import { Product } from '../../home/models/Product';
 
 @Component({
   selector: 'productpage-filter',
@@ -6,10 +8,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./filter.component.scss']
 })
 export class FilterComponent {
+  @Output() public filteredProducts = new EventEmitter<{ search: string, categoryId: number | null, price: number }>();
+
+  products: Product[] = [];
+  search: string = '';
+  categoryId: number | null = null;
   minPrice: number = 0;
   priceValue: number = 100;
 
-  constructor(){}
+  constructor(private productService: ProductService) { }
+
+
+  applyFilters(event: Event) {
+    this.filteredProducts.emit({ search: this.search, categoryId: this.categoryId, price: this.priceValue });
+  }
+
+  clearFilters(event: Event) {
+    event.preventDefault();
+    this.search = '';
+    this.categoryId = null;
+    this.priceValue = 100;
+    this.filteredProducts.emit({ search: this.search, categoryId: this.categoryId, price: this.priceValue });
+  }
 
   validatePrice(event: Event): void {
     const inputElement = event.target as HTMLInputElement;
