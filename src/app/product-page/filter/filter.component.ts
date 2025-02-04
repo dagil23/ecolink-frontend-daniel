0c9f1e2d3e1864ea3bac41c076fe229217cb6387
CarlosBertomeuC <carlos.bertomeu@iesdoctorbalmis.com>
2025-02-04 12:26:03 +0100

    feat: enhance filter component with price validation and improved UI

--- src/app/product-page/filter/filter.component.ts
+++ src/app/product-page/filter/filter.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrl: './filter.component.scss'
 })
 export class FilterComponent {
+  minPrice: number = 0;
+  priceValue: number = 100;
 
+  constructor(){}
+
+  validatePrice(value: number): void {
+    if (value > 1000) {
+      this.priceValue = 1000;
+    } else if (value < 0) {
+      this.priceValue = 0;
+    } else {
+      this.priceValue = value;
+    }
+  }
 }
