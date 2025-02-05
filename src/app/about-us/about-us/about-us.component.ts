aa58ed0afd4f419dfe298b29b627c33fa1a2ca53
CarlosBertomeuC <carlos.bertomeu@iesdoctorbalmis.com>
2025-02-05 13:39:28 +0100

    feat: add Leaflet map integration and update dependencies in about us component

--- src/app/about-us/about-us/about-us.component.ts
+++ src/app/about-us/about-us/about-us.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-about-us',
   templateUrl: './about-us.component.html',
-  styleUrl: './about-us.component.scss'
+  styleUrls: ['./about-us.component.scss']
 })
 export class AboutUsComponent {
+  // export class AboutUsComponent implements AfterViewInit {
 
+  constructor() { }
+
+  // ngAfterViewInit(): void {
+  //   this.initMap();
+  // }
+
+  // private initMap(): void {
+  //   const map = L.map('map').setView([38.361895, -0.491099], 13);
+
+  //   L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  //     attribution: '© OpenStreetMap contributors'
+  //   }).addTo(map);
+
+  //   L.marker([38.361895, -0.491099]).addTo(map)
+  //     .bindPopup('Our location<br> <a href="/index">Ecolink Office</a>')
+  //     .openPopup();
+  // }
 }
