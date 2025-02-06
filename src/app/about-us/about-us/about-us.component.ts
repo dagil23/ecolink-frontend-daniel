e49ac97b1216bc31abb2966ab510de26ee41c12e
CarlosBertomeuC <carlos.bertomeu@iesdoctorbalmis.com>
2025-02-06 09:54:07 +0100

    refactor: remove Leaflet map implementation and related styles from about us component

--- src/app/about-us/about-us/about-us.component.ts
+++ src/app/about-us/about-us/about-us.component.ts
@@ -1,5 +1,4 @@
-import { Component, AfterViewInit } from '@angular/core';
-import * as L from 'leaflet';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-about-us',
@@ -7,24 +6,7 @@ import * as L from 'leaflet';
   styleUrls: ['./about-us.component.scss']
 })
 export class AboutUsComponent {
-  // export class AboutUsComponent implements AfterViewInit {
 
   constructor() { }
 
-  // npm install leaflet @types/leaflet
-  // ngAfterViewInit(): void {
-  //   this.initMap();
-  // }
-
-  // private initMap(): void {
-  //   const map = L.map('map').setView([38.361895, -0.491099], 13);
-
-  //   L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  //     attribution: '© OpenStreetMap contributors'
-  //   }).addTo(map);
-
-  //   L.marker([38.361895, -0.491099]).addTo(map)
-  //     .bindPopup('Our location<br> <a href="/index">Ecolink Office</a>')
-  //     .openPopup();
-  // }
 }
