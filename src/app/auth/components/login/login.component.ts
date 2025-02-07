bec11a70f80ba46964dfddde2fe64674c7dff171
CristianCarbonell <cristian2306c@gmail.com>
2025-02-07 12:43:22 +0100

    Removed email validators temporally for testing.

--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -21,7 +21,7 @@ export class LoginComponent implements OnInit {
 
   ngOnInit(): void {
     this.loginForm = this.fb.group({
-      email: ['', [Validators.required, Validators.email]],
+      email: ['',],
       password: ['', [Validators.required, Validators.minLength(8)]],
     });
   }
