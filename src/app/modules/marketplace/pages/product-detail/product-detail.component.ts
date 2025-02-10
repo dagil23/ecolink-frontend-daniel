d93efd378cb4e6ac4beb4dc4fe1a5b3390d2608b
CarlosBertomeuC <carlos.bertomeu@iesdoctorbalmis.com>
2025-02-10 15:01:03 +0100

    Enhance product detail view; implement product retrieval by ID; update product card and challenges layout

--- src/app/modules/marketplace/pages/product-detail/product-detail.component.ts
+++ src/app/modules/marketplace/pages/product-detail/product-detail.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductService } from '../../services/product.service';
+import { Product } from '../../../../core/models/Product';
 
 @Component({
   selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
   styleUrl: './product-detail.component.scss'
 })
-export class ProductDetailComponent {
+export class ProductDetailComponent implements OnInit {
+  productId: string | null = null;
+  product!: Product;
 
+  constructor(private route: ActivatedRoute, private productService: ProductService) { }
+
+  ngOnInit(): void {
+    this.productId = this.route.snapshot.paramMap.get('id');
+    this.productService.getProductById(this.productId).subscribe((product: Product) => {
+      this.product = product;
+    });
+  }
 }
