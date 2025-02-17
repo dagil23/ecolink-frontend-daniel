a1311548aa50057bbbccc9e882d346cb8dda12e7
CristianCarbonell <cristian2306c@gmail.com>
2025-02-17 02:19:58 +0100

    Completed StartupProposal service.

--- src/app/modules/startup-dashboard/services/StartupProposals.service.ts
+++ src/app/modules/startup-dashboard/services/StartupProposals.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
+import {Proposal} from '../../startups/models/Proposal';
 
 @Injectable({
   providedIn: 'root'
 })
-export class StartupProposalsService {
+export class ProposalService {
+  private proposalUrl = `${environment.apiUrl}/proposal`;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getProposalsByChallenge(id: number): Observable<Proposal[]> {
+    return this.http.get<Proposal[]>(`${this.proposalUrl}/challenge/${id}`);
+  }
+
+  addProposal(id: number, formData: FormData): Observable<Proposal> {
+    return this.http.post<Proposal>(`${this.proposalUrl}/challenge/${id}`, formData);
+  }
+
+  updateProposal(formData: FormData): Observable<Proposal> {
+    const id = formData.get('id');
+    return this.http.put<Proposal>(`${this.proposalUrl}/${id}`, formData);
+  }
+
+  deleteProposal(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.proposalUrl}/${id}`);
+  }
 }
