15b7988c8267937c7901ef9de00329f7af56cc32
Aarón Carrera <aarcarpas@gmail.com>
2025-02-20 22:51:15 +0100

    Add configuration for company and startup

--- src/app/core/guards/company.guard.ts
+++ src/app/core/guards/company.guard.ts
@@ -1,5 +1,40 @@
-import { CanActivateFn } from '@angular/router';
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, CanActivateFn, GuardResult, MaybeAsync, Router, RouterStateSnapshot } from '@angular/router';
+import { AuthService } from '../../auth/services/AuthService.service';
+import { catchError, map, of, switchMap } from 'rxjs';
+import { User } from '../models/User';
 
-export const companyGuard: CanActivateFn = (route, state) => {
-  return true;
-};
+@Injectable({
+  providedIn: 'root'
+})
+export class CompanyGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) { }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): MaybeAsync<GuardResult> {
+    return this.authService.isAuthenticated().pipe(
+      switchMap(isLogged => {
+        const rutaActual = state.url;
+
+        if (!isLogged) {
+          return of(false);
+        }
+
+        return this.authService.getCurrentUser().pipe(
+          map((user: User) => {
+            console.log(user);
+            if (user.userType.toUpperCase() !== 'COMPANY') {
+              this.router.navigate(['/']);
+              return false;
+            }
+            return true;
+          }),
+          catchError(() => {
+            this.router.navigate(['/']);
+            return of(false);
+          })
+        );
+      })
+    );
+  }
+
+}
--- src/app/modules/company-dashboard/company-dashboard-routing.module.ts
+++ src/app/modules/company-dashboard/company-dashboard-routing.module.ts
@@ -3,23 +3,28 @@ import { RouterModule, Routes } from '@angular/router';
 import { CompanyChallengesComponent } from './pages/company-challenges/company-challenges.component';
 import { CompanyChallengeFormComponent } from './pages/company-challenge-form/company-challenge-form.component';
 import { CompanyProposalsComponent } from './pages/company-proposals/company-proposals.component';
+import { CompanyGuard } from '../../core/guards/company.guard';
 
 const routes: Routes = [
   {
     path: 'challenges',
     component: CompanyChallengesComponent,
+    canActivate: [CompanyGuard],
   },
   {
     path: 'challenges/new',
     component: CompanyChallengeFormComponent,
+    canActivate: [CompanyGuard],
   },
   {
     path: 'challenges/edit/:id',
     component: CompanyChallengeFormComponent,
+    canActivate: [CompanyGuard],
   },
   {
     path: 'challenges/proposal/:id',
     component: CompanyProposalsComponent,
+    canActivate: [CompanyGuard],
   },
   {
     path: '**',
