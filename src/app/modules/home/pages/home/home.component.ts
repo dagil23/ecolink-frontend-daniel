75c2ea344d62b741b671f37a2aba01658327ec0f
CarlosBertomeuC <carlos.bertomeu@iesdoctorbalmis.com>
2025-02-20 09:43:02 +0100

    Implement navigation logic based on user authentication; update product card display

--- src/app/modules/home/pages/home/home.component.ts
+++ src/app/modules/home/pages/home/home.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../../../auth/services/AuthService.service';
+import { User } from '../../../../core/models/User';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  constructor(private authService: AuthService, private router: Router) {}
+
+  isLogged: boolean = false;
+
+  ngOnInit(): void {
+      this.authService.getCurrentUser().subscribe((user: User) => {
+            this.isLogged = !!user;
+      });
+    }
+    navigate(): void {
+      if (this.isLogged) {
+        this.router.navigate(['/profile']);
+      } else {
+        this.router.navigate(['/auth/login']);
+      }
+    }
 }
